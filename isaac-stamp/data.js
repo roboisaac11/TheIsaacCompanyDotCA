fb013983c339f5efd2256ef6b8fa305db7768796
Isaac Kroeker <isaackroeker@gmail.com>
2025-07-22 22:09:05 -0500

    Isaac stamp of approval

---
+++ isaac-stamp/data.js
@@ -0,0 +1,18 @@
+
+const stampData = [
+  {
+    uuid: "53b8b672",
+    name: "n8n",
+    summary: "A great automation tool that you can host on prem or use in the cloud. Intregrates with pretty much everything and is super simple and very powerful.",
+    date: "2025-07-22",
+    link: "https://n8n.io/"
+  },
+  {
+    uuid: "9e25cee6",
+    name: "Sour Patch Kids",
+    summary: "W candy.",
+    date: "2025-07-22",
+    link: "https://www.snackworks.com/brands/sour-patch-kids/"
+  },
+
+];
